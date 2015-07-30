bef8a5f683c69a54dcc6f2928fb0e058d1088274
cynthiatliu <cynthia.liu@nist.gov>
2015-07-30 11:15:24 -0400

    Update file formatter.py

--- file formatter.py
+++ file formatter.py
@@ -2,10 +2,12 @@
 #Interactive hkl file formatting!
 
 import math
+import re
+import string
 
 #Main
 def main():
-    jobs = [0, 1] #Will increase as I get more types of jobs
+    jobs = [0, 1, 2] #Will increase as I get more types of jobs
     
     #While the user wants to continue
     while True:
@@ -13,7 +15,7 @@ def main():
         good = False
         while good != True:
             try:
-                job = int(eval(raw_input("What is the original appearance of the file you want to format? (Choose 0 to quit) \n\n1. Each line has x characters before hkl begins, and all data lines include hkl \n2. Under construction, do not select.\n")))
+                job = int(eval(raw_input("What is the original appearance of the file you want to format? (Choose 0 to quit) \n\n1. Each line has x characters before hkl begins, and all data lines include hkl \n2. Sometimes when a positive number is followed by a negative number there is no space between the first number and the dash.\n3. Under construction; do not select.\n")))
                 if job in jobs: good = True
             except:
                 print ("Invalid input (must be a number corresponding to the original appearance) - try again?")
@@ -23,7 +25,7 @@ def main():
             break
                 
         #Giant switch
-        if job == 1:
+        if job == 1: #stuff before the hkl
             #File name
             good = False
             while good != True:
@@ -65,6 +67,39 @@ def main():
             
             print ("Done!")
             
+        elif job == 2: #dashes connecting stuff that shouldn't be connected
+            #File name
+            good = False
+            while good != True:
+                try:
+                    name = raw_input("What is the name of the file you want to read? Don't add an extension. ") + ".txt"
+                    toEdit = open(name, 'r')
+                    good = True
+                except:
+                    print ("That can't be the right file! Please try again")
+                    
+            #File name to write to
+            good = False
+            while good != True:
+                try:
+                    name2 = raw_input("What is the name of the file you want to write to? ") + ".txt"
+                    edited = open(name2, 'w')
+                    good = True
+                except:
+                    print ("You've got invalid letters in your file name! Take 'em out!")
+                    
+            line = toEdit.readline()
+            while line != '':
+                patt = re.compile(r'(\d+)-(\d+)')
+                matches = patt.findall(line)
+                for i in range(len(matches)):
+                    stri = matches[i][0] + "-" + matches[i][1]
+                    line = line.replace(stri, stri[0:string.rfind(stri, "-")] + " " + stri[string.rfind(stri, "-"):])
+                edited.write(line)
+                line = toEdit.readline()
+                
+            print ("Done!")
+                    
         else: print ("Still in the works. Check back later?")
         
 main()
